99481a4a322b9e75a3eaf438f7636daf123b6c82
Sergey Mironov <sergeymironov0001@gmail.com>
2017-10-25 21:40:00 +0300

    Add main application class.

---
+++ memorize-helper-backend/src/main/java/org/sergeymironov0001/memhelper/Application.java
@@ -0,0 +1,39 @@
+package org.sergeymironov0001.memhelper;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Arrays;
+
+
+@SpringBootApplication
+@Slf4j
+public class Application {
+
+    public static void main(String[] args) throws UnknownHostException {
+        SpringApplication application = new SpringApplication(Application.class);
+        ConfigurableApplicationContext run = application.run(args);
+        log.info(getStartupInfo(run.getEnvironment()));
+    }
+
+    private static String getStartupInfo(Environment env) throws UnknownHostException {
+        return String.format(
+                "Access URLs:\n----------------------------------------------------------\n\t" +
+                        "Service: \t%s\n\t" +
+                        "Local: \t\thttp://127.0.0.1:%s\n\t" +
+                        "External: \thttp://%s:%s\n\t" +
+                        "Profiles: \t%s\n\t" +
+                        "----------------------------------------------------------\n\t",
+                env.getProperty("spring.application.name"),
+                env.getProperty("server.port"),
+                InetAddress.getLocalHost().getHostAddress(),
+                env.getProperty("server.port"),
+                Arrays.toString(env.getActiveProfiles())
+        );
+    }
+}
